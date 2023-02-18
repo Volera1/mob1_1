52127e0ca744815bbe3005aa81ce5bf3b82725c4
Валерия Воликова <lerusic2002@rambler.ru>
2023-02-18 18:10:38 +0800

    Отдельный класс для тестирования

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,19 @@ import java.util.*
 
 fun main(args: Array<String>) {
     println("Введите предложение:")
-    val scanner = Scanner(System.`in`)// прочитать всю строку
-    val line = scanner.nextLine()
+    val scan = Scanner(System.`in`)
+    val line = scan.nextLine()
+
+    val result = stringChange(line)
+    println(result)
+}
+fun stringChange(line:String):String {
     val list = line.split(' ')
-    var result = arrayListOf<String>();
+    val result = arrayListOf<String>()
     for (word in list){
         if (word.length%2!=0){
             result += word.reversed()
         }
     }
-    println(result)
+    return(result.toString())
 }
